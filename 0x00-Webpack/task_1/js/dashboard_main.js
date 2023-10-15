050f90b6ff75ca1497dd02756652a836de607b0d
John Macharia <machariajohn23@gmail.com>
2023-10-15 19:09:33 +0300

    Entry point for the task_1 program

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,20 @@
 const $ = require('jquery');
 const _ = require('lodash');
 
-let counter = 0;
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
-$("<p>Holberton Dashboard</p>").appendTo('body');
-$("<p>Dashboard data for the students</p>").appendTo('body');
-$("<button>Click here to get started</button>").appendTo('body');
-$("<p id='count'></p>").appendTo('body');
-$("<p>Copyright - Holberton School</p>").appendTo('body');
+let count = 0;
 
 function updateCounter() {
-  counter++;
-  $('#count').html(`${counter} clicks on the button`);
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-$("button").on("click", _.debounce(updateCounter, 500));
+const $button = $('<button>Click here to get started</button>').on(
+  'click',
+  _.debounce(updateCounter, 500, { leading: true, trailing: false })
+);
