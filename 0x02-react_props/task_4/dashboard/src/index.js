ac200ec7a58b565b326360d232e317471a0cd9de
John Macharia <machariajohn23@gmail.com>
2023-10-23 18:01:01 +0300

    Entry point of the app

---
+++ 0x02-react_props/task_4/dashboard/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+//import Notifications from './Notifications';
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<Notifications />, document.getElementById('root-notifications'))
