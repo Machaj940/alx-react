686a09b33d30db26d97d3f6d2310288235e7d927
John Macharia <machariajohn23@gmail.com>
2023-10-28 18:28:15 +0300

    Style Footer component using Aphrodite

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -0,0 +1,27 @@
+import { getFullYear, getFooterCopy } from '../utils';
+import { StyleSheet, css } from 'aphrodite';
+//import './Footer.css'
+
+function Footer() {
+  const year = getFullYear();
+  const footerText = getFooterCopy(true);
+
+  return (
+    <div className={css(styles.Footer)}>
+      <p>Copyright {year} - {footerText}</p>
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({
+  Footer: {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    borderTop: '3px solid #FF5A5F',
+  },
+});
+
+export default Footer
